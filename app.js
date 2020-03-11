1920b9634ed706333c8c2933b63c7accede7b7f2
aldiwildan77 <Aldiwild77@gmail.com>
2020-03-11 23:38:55 +0700

    update api dataset for checker and listing

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5015;
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const DATASET_PATH = path.dirname('/var/www/handwritten/uploads/');
+const DATASET_PATH = 'yourpath';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -19,6 +19,10 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     let length = checkDirectoryFilesLength(file);
     let fileName = checkDirectory(file);
+    if (length > 10) {
+      const error = new Error('Only 10 files are allowed');
+      return cb(error, false);
+    }
     cb(null, `${fileName}_${length}.png`);
   }
 });
@@ -62,19 +66,95 @@ const uploadField = upload.fields([{ name: 'default', maxCount: 4 }]);
 // Routes
 app.get('/', (req, res, next) => {
   res.status(200).json({
-    message: 'Hello World'
+    message: 'Hello World',
   });
 });
 
 app.post('/api/dataset', (req, res, next) => {
-  console.log('POST /api/dataset');
   uploadField(req, res, (err) => {
     if (err) return next(err);
     res.status(200).json({
       status: 'success',
-      message: 'Image uploaded'
+      message: 'Image uploaded',
     });
   });
+});
+
+app.get('/api/dataset/checker', (req, res, next) => {
+  const { fileName } = req.query;
+  const directoryPath = path.join(DATASET_PATH + '/uploads/');
+  try {
+    if (!fileName) {
+      const error = new Error('Link should has fileName query');
+      error.statusCode = 422;
+      return next(error);
+    }
+
+    let { length } = fs.readdirSync(directoryPath + fileName);
+    if (length == 10) {
+      const error = new Error('Directory is full');
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Filename accepted',
+    });
+  } catch (err) {
+    const error = new Error('Internal Server Error');
+    next(error);
+  }
+});
+
+app.get('/api/dataset', (req, res, next) => {
+  const { length, paginate } = req.query;
+  const directoryPath = path.join(DATASET_PATH + '/uploads/');
+  try {
+    let result = fs.readdirSync(directoryPath);
+    let resultWithLength = [];
+    let errors = [];
+
+    if (length < 0 || length > 10) {
+      errors.push({
+        type: 'Length Query',
+        message: 'Length query accepted those are between 0 - 10',
+      });
+    }
+
+    if (paginate < 0) {
+      errors.push({
+        type: 'Paginate Query',
+        message: 'Paginate query accepted are more than or equal to 0',
+      });
+    }
+
+    if (errors.length) {
+      return res.status(400).json({
+        status: 'error',
+        errors,
+      });
+    }
+
+    result.forEach(directory => {
+      let innerDirectory = fs.readdirSync(directoryPath + directory);
+      if (resultWithLength.length >= paginate) return;
+      if (!length || innerDirectory.length == length) {
+        resultWithLength.push({
+          fileName: directory,
+          length: innerDirectory.length,
+        });
+      }
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: resultWithLength,
+    });
+  } catch (err) {
+    const error = new Error('Internal Server Error');
+    next(error);
+  }
 });
 
 // Error Handlers
